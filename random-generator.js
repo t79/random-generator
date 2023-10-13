f840474a03a4fd501a1c5b6e21f87ea881d8ba25
Terje Urnes <terje@urnes.net>
2023-10-13 19:08:39 +0200

    Setting up simple gui

---
+++ random-generator.js
@@ -0,0 +1,11 @@
+document.addEventListener('readystatechange', function () {
+    if (document.readyState === 'interactive') {
+        initGenerator();
+    }
+});
+
+var t79RG = {};
+
+function initGenerator() {
+    
+}
